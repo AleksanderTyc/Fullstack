cc99f67fa8aafe743f6fa048152ee474e7975078
Aleks Tyc <aleks.tyc@gmail.com>
2025-10-22 19:51:01 +0200

    Ex_0023 JS J.1.16. People counter - buttons

--- Ex_0023/index.js
+++ Ex_0023/index.js
@@ -1,1 +1,13 @@
 console.log("This is index.js");
+let count = 0;
+console.log(`Counter is ${count}`);
+
+
+const increment = () => {
+    count += 1;
+    document.getElementById("count-el").innerText = count;
+}
+
+const save = () => {
+    console.log(`Save function * counter is ${count}`);
+}
