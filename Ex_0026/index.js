e9ba15e02040922d8d78257684c322361860c7e2
Aleks Tyc <aleks.tyc@gmail.com>
2025-10-25 17:30:57 +0200

    Ex_0026 JS J.4.14. Blackjack start game button

--- Ex_0026/index.js
+++ Ex_0026/index.js
@@ -1,20 +1,24 @@
 let firstCard = 10;
 let secondCard = 11;
 
-let sum = firstCard +secondCard;
+let sum = firstCard + secondCard;
 
 let hasBlackjack = false;
 let isAlive = true;
 let message = "";
 
-if( sum < 21) {
-message = "Do you want to draw another card?";
-} else if( sum === 21) {
-    hasBlackjack = true;
-    message = "You've got Blackjack!";
-} else {
-    isAlive = false;
-    message = "You're out of the game!";
-}
+const startGame = () => {
+    console.log("startGame clicked");
 
-console.log(`Sum: ${sum} ${message} hasBlackjack: ${hasBlackjack} isAlive: ${isAlive}`);
+    if (sum < 21) {
+        message = "Do you want to draw another card?";
+    } else if (sum === 21) {
+        hasBlackjack = true;
+        message = "You've got Blackjack!";
+    } else {
+        isAlive = false;
+        message = "You're out of the game!";
+    }
+
+    console.log(`Sum: ${sum} ${message} hasBlackjack: ${hasBlackjack} isAlive: ${isAlive}`);
+}
