f33156630c5ab63551f8950a2b34cdf10d4e5cc7
Aleks Tyc <aleks.tyc@gmail.com>
2025-10-25 20:02:56 +0200

    Ex_0026 JS J.4.44. Blackjack game logic

--- Ex_0026/index.js
+++ Ex_0026/index.js
@@ -1,24 +1,68 @@
-let firstCard = 10;
-let secondCard = 11;
-
-let sum = firstCard + secondCard;
-
+let card = 0;
+let sum = 0;
 let hasBlackjack = false;
 let isAlive = true;
-let message = "";
+
+const getBlackjackCard = () => {
+    const randomCard = Math.floor(Math.random() * 13) + 2;
+    return randomCard > 11 ? 10 : randomCard;
+}
 
 const startGame = () => {
     console.log("startGame clicked");
+    document.getElementById("cards-el").textContent = "Cards:";
+    document.getElementById("sum-el").textContent = "Sum:";
+    card = 0;
+    sum = 0;
+
+    // card = Math.floor(Math.random() * 10) + 2; // 2-11
+    card = getBlackjackCard();
+    sum += card;
+    presentCard();
+    // card = Math.floor(Math.random() * 10) + 2; // 2-11
+    card = getBlackjackCard();
+    sum += card;
+    presentCard();
+
+
+    // let firstCard = Math.floor( Math.random() *10 ) +2; // 2-11
+    // let secondCard = Math.floor( Math.random() *10 ) +2; // 2-11
+
+    hasBlackjack = false;
+    isAlive = true;
+
+    renderGame();
+}
+
+const renderGame = () => {
+    console.log("renderGame clicked");
 
     if (sum < 21) {
-        message = "Do you want to draw another card?";
+        messageEl = "Do you want to draw another card?";
     } else if (sum === 21) {
         hasBlackjack = true;
-        message = "You've got Blackjack!";
+        messageEl = "You've got Blackjack!";
     } else {
         isAlive = false;
-        message = "You're out of the game!";
+        messageEl = "You're out of the game!";
     }
 
-    console.log(`Sum: ${sum} ${message} hasBlackjack: ${hasBlackjack} isAlive: ${isAlive}`);
+    console.log(`Sum: ${sum} ${messageEl} hasBlackjack: ${hasBlackjack} isAlive: ${isAlive}`);
+    document.getElementById("message-el").textContent = messageEl;
 }
+
+const newCard = () => {
+    console.log("newCard clicked");
+    // card = Math.floor(Math.random() * 10) + 2; // 2-11
+    card = getBlackjackCard();
+    sum += card;
+    presentCard();
+    renderGame();
+}
+
+function presentCard() {
+    // const presentCard = () => {
+    document.getElementById("cards-el").textContent += " ".concat(card);
+    document.getElementById("sum-el").textContent = "Sum: ".concat(sum);
+}
+// renderGame();
