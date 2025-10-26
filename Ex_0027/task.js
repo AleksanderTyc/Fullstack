1575eec4a5d751dd42f7231faec4d604b69a9b31
Aleks Tyc <aleks.tyc@gmail.com>
2025-10-26 21:28:02 +0100

    Ex_0027 JS J.5. JS Simple examples

---
+++ Ex_0027/task.js
@@ -0,0 +1,42 @@
+/* Person object */
+console.log("This is task");
+
+const personObj = {
+    name: "Alex",
+    age: 51,
+    country: "Poland"
+}
+
+const logData = (arg) => { return `${arg.name} is ${arg.age} years old and lives in ${arg.country}`; }
+
+console.log(logData(personObj));
+
+
+/* Discount */
+const detDiscount = (age) => {
+    if( age < 6 ) return "free";
+    if( age < 18 ) return "child discount";
+    if( age < 27 ) return "student discount";
+    if( age < 67 ) return "full price";
+    return "senior citizen discount";
+}
+
+for (let index = 5; index < 75; index+=5){
+    console.log( `Age is ${index}, discount: ${detDiscount(index)}`);
+}
+
+/* Large countries */
+const largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"];
+largeCountries.map((elem,index) => {console.log(`- ${index}: ${elem}`);});
+
+/* Large countries - broken */
+let largeCountriesB = ["Tuvalu", "India", "USA", "Indonesia", "Monaco"];
+console.log( `largeCountriesB start: ${largeCountriesB}`);
+largeCountriesB.pop();
+console.log( `largeCountriesB remove Monaco: ${largeCountriesB}`);
+largeCountriesB.push("Pakistan")
+console.log( `largeCountriesB add Pakistan: ${largeCountriesB}`);
+largeCountriesB.shift();
+console.log( `largeCountriesB remove Tuvalu: ${largeCountriesB}`);
+largeCountriesB.unshift("China");
+console.log( `largeCountriesB add China: ${largeCountriesB}`);
