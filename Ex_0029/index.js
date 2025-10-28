7e3a8e91c952be40087bba0771f05a59f6a3a858
Aleks Tyc <aleks.tyc@gmail.com>
2025-10-28 15:14:20 +0100

    Ex_0029 JS J.6.11. Handling button click

--- Ex_0029/index.js
+++ Ex_0029/index.js
@@ -1,4 +1,11 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
 
 function saveInput() {
     console.log("saveInput clicked");
+    console.log(`inputEl value is ${inputEl.value}`);
+    if( inputEl.value.length > 0 ) { myLeads.push(inputEl.value); }
+    console.log(`myLeads value is ${myLeads}`);
 }
+
+document.getElementById("input-btn").addEventListener("click",saveInput);
