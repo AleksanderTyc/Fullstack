65ad07b67e53964fba940973c33f913d467f7069
Aleks Tyc <aleks.tyc@gmail.com>
2025-10-28 16:51:31 +0100

    Ex_0029 JS J.6.14. Unordered list my attempt

--- Ex_0029/index.js
+++ Ex_0029/index.js
@@ -1,11 +1,21 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 
+const ulEl = document.getElementById("ul-el");
+console.log(ulEl);
+
 function saveInput() {
     console.log("saveInput clicked");
     console.log(`inputEl value is ${inputEl.value}`);
-    if( inputEl.value.length > 0 ) { myLeads.push(inputEl.value); }
-    console.log(`myLeads value is ${myLeads}`);
+    if (inputEl.value.length > 0) {
+        myLeads.push(inputEl.value);
+        ulEl.innerHTML += `<li>${inputEl.value}</li>`;
+    }
+    console.log(`myLeads line by line:`);
+    myLeads.map((elem, indx) => {
+        console.log(`${indx}. ${elem}`);
+    });
+    console.log(`myLeads line by line end`);
 }
 
-document.getElementById("input-btn").addEventListener("click",saveInput);
+document.getElementById("input-btn").addEventListener("click", saveInput);
