41e15a4aaa16d636af8e413ac5eca92ef2552b47
Aleks Tyc <aleks.tyc@gmail.com>
2025-11-05 10:19:41 +0100

    Ex_0030 JS J.7.9. Solo - Unit converter logic

--- Ex_0030/index.js
+++ Ex_0030/index.js
@@ -7,11 +7,20 @@
 
 
 function convertUnits() {
-    const valueHeroInput = document.getElementById("hero-input").value;
-    const valueHeroInputNum = Number(valueHeroInput);
+    const valueHeroInputNum = Number(document.getElementById("hero-input").value);
     const pLengthHTML = valueHeroInputNum ?
-        `${valueHeroInput} meters = ${(valueHeroInput * 3.281).toFixed(3)} feet | ${valueHeroInput} feet = ${(valueHeroInput / 3.281).toFixed(3)} meters`
+        `${valueHeroInputNum} meters = ${(valueHeroInputNum * 3.281).toFixed(3)} feet | ${valueHeroInputNum} feet = ${(valueHeroInputNum / 3.281).toFixed(3)} meters`
         : "Input is not a number";
     document.getElementById("p-length").innerHTML = pLengthHTML;
     document.getElementById("p-length").style.color = "#000";
+    const pVolumeHTML = valueHeroInputNum ?
+        `${valueHeroInputNum} liters = ${(valueHeroInputNum * 0.264).toFixed(3)} gallon | ${valueHeroInputNum} gallon = ${(valueHeroInputNum / 0.264).toFixed(3)} liters`
+        : "Input is not a number";
+    document.getElementById("p-volume").innerHTML = pVolumeHTML;
+    document.getElementById("p-volume").style.color = "#000";
+    const pMassHTML = valueHeroInputNum ?
+        `${valueHeroInputNum} kilograms = ${(valueHeroInputNum * 2.204).toFixed(3)} pound | ${valueHeroInputNum} pound = ${(valueHeroInputNum / 2.204).toFixed(3)} kilograms`
+        : "Input is not a number";
+    document.getElementById("p-mass").innerHTML = pMassHTML;
+    document.getElementById("p-mass").style.color = "#000";
 }
