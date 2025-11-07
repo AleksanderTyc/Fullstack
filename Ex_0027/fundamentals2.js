b1c3f4450f0a2dd8e124f1b8131a158280127f45
Aleks Tyc <aleks.tyc@gmail.com>
2025-11-07 19:09:59 +0100

    Ex_0027 JS Fundamentals - arrow functions, array.reduce

---
+++ Ex_0027/fundamentals2.js
@@ -0,0 +1,56 @@
+
+/* Which of the 3 definitions will work with thic invocation? */
+// console.log(getSpendAlert(10));
+
+/* OK - Hoisting standard */
+// function getSpendAlert(amount) {
+//     return `Warning *** You just spent ${amount}!`;
+// }
+
+/* Cannot access before initialisation */
+// const getSpendAlert = function (amount) {
+//     return `Warning *** You just spent ${amount}!`;
+// }
+
+/* Cannot access before initialisation */
+// const getSpendAlert = (amount) => {
+//     return `Warning *** You just spent ${amount}!`;
+// }
+
+/* The above syntax is equivalent to */
+// const getSpendAlert = amount => `Warning *** You just spent ${amount}!`;
+/* This is because:
+    1. We have exactly one parameter.
+    2. The only expression calculated in the function is the returned value.
+*/
+
+/* This works for all of them */
+// console.log(getSpendAlert(10));
+
+// const speedWarning = speed => `You are going at ${speed} mph!`;
+console.log('speedWarning');
+const speedWarning = (speedLimit, speedActual) => speedLimit < speedActual ? `You are going at ${speedActual} mph!` : '';
+console.log(speedWarning(50, 40)); // Empty line
+console.log(speedWarning(30, 40));
+
+console.log('distanceTravelled');
+const distanceTravelledMiles = [267, 345, 234, 190, 299];
+const distanceTravelledKm = distanceTravelledMiles.map(distance => Math.round(distance * 1.601));
+console.log(distanceTravelledKm);
+
+
+console.log('Total distance');
+const { interDestsArr, localDestsArr } = require('./destinations.js');
+function calculateTotalDistance(destArray) {
+    // This does not work, because the array elements are not numbers.
+    // const total = destArray.reduce((runTot, runCurr) => runTot+runCurr.distanceKM);
+    // We have to explicitly provide the initial value, as per documentation:
+    // Normally, array element 0 is used as initial value, and the iteration starts from array element 1.
+    // If an initial value is supplied, this is used, and the iteration starts from array element 0.
+    const total = destArray.reduce((runTot, runCurr) => runTot + runCurr.distanceKM, 0);
+    return total;
+}
+// This would work with the first attempt - calling reduce without initial value.
+// console.log( `distanceTravelledKm: ${calculateTotalDistance(distanceTravelledKm)}`);
+console.log(`interDestsArr: ${calculateTotalDistance(interDestsArr)}`);
+console.log(`localDestsArr: ${calculateTotalDistance(localDestsArr)}`);
