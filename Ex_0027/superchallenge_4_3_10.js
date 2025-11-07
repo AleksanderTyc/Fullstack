c3591f6a43b328ab7bc40a2d0ba7116737acb68b
Aleks Tyc <aleks.tyc@gmail.com>
2025-11-07 20:18:50 +0100

    Ex_0027 JS Fundamentals - Properties superchallenge - tidy up

--- Ex_0027/superchallenge_4_3_10.js
+++ Ex_0027/superchallenge_4_3_10.js
@@ -3,12 +3,13 @@
 const { placeholderPropertyObj } = require('./properties/placeholderPropertyObj.js');
 const { propertyForSaleArr } = require('./properties/propertyForSaleArr.js');
 
-
+// When placeholder is just an Object, not an Array of a single Object:
+// function getPropertyHTML(propertyArr = [placeholderPropertyObj]) {
 function getPropertyHTML(propertyArr = placeholderPropertyObj) {
     return propertyArr.map(
         property => {
             const { propertyLocation, priceGBP, roomsM2, comment, image } = property;
-            const totalSize = roomsM2.reduce( (totSize, curSize) => totSize+curSize, 0 );
+            const totalSize = roomsM2.reduce((totSize, curSize) => totSize + curSize, 0);
             return `
 <section class="card">
     <img src="/images/${image}">
@@ -24,42 +25,7 @@ function getPropertyHTML(propertyArr = placeholderPropertyObj) {
     ).join(' ');
 }
 
-// const templateHTML = `
-// <section class="card">
-//     <img src="/images/${IMAGE}">
-//     <div class="card-right">
-//         <h2>${PRPERTYLOCATION}</h2>
-//         <h3>${PRICE}</h3>
-//         <p>${COMMENT}</p>
-//         <h3>${TOTALSIZE}</h3>
-//     </div>
-// </section>
-// `;
-
 console.log('No parameters passed');
 console.log(getPropertyHTML());
 console.log('propertyForSaleArr passed');
 console.log(getPropertyHTML(propertyForSaleArr));
-
-
-/* Rubbish */
-/*
-    return propertyArr.map(
-        property => {
-            const { propertyLocation, priceGBP, roomsM2, comment, image } = property;
-            const totalSize = roomsM2.reduce( (totSize, curSize) => totSize+curSize, 0 );
-            return
-`
-<section class="card">
-    <img src="/images/${image}">
-    <div class="card-right">
-        <h2>${propertyLocation}</h2>
-        <h3>${priceGBP}</h3>
-        <p>${comment}</p>
-        <h3>${totalSize}</h3>
-    </div>
-</section>
-`;
-        }
-    ).join(' ');
-*/
