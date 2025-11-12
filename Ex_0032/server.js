ddb0afb97887531926d9e1c3d0b808549dcf9f5b
Aleks Tyc <aleks.tyc@gmail.com>
2025-11-12 19:34:55 +0100

    Ex_0032 Node npm start

--- Ex_0032/server.js
+++ Ex_0032/server.js
@@ -1,1 +1,4 @@
-console.log( 'Hello Node!' );
+// package.json > scripts.start shows the command to run the project, here it is `node server.js`
+// So instead of running `$ node server.js` we can run `$ npm start` with the same effect.
+
+console.log( 'Hello Node!\nWe have package.json' );
