f76b11ddeaf322fe476eeaa382e18086c8508ed2
Aleks Tyc <aleks.tyc@gmail.com>
2025-11-15 22:03:06 +0100

    Ex_0033 FS read synchronous and async first take

--- Ex_0033/server.js
+++ Ex_0033/server.js
@@ -16,25 +16,51 @@ const __dirname = import.meta.dirname;
 import { serveStatic } from './utils/serveStatic.js';
 console.log('serveStatic', serveStatic(__dirname));
 
+import fs from 'node:fs';
+
 const server = http.createServer((req, res) => {
     const pathToResource = path.join(import.meta.dirname, 'public', 'index.html');
-    console.log('pathToResource', pathToResource);
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
-    res.statusCode = 200;
-    // Alternatively:
-    /*
-    res.writeHead(
-        200,
-        {
-            'Content-Type': 'text/html; charset=utf-8'
+
+    // First method, we don't use it:
+    // const content = fs.readFileSync(pathToResource, 'utf8');
+    // Problem with this: ...Sync - it is blocking, we prefer asynchronous
+
+    // Second method, asynchronous
+    fs.readFile(pathToResource, 'utf8', (err, content) => {
+        if (err) {
+            console.error(err);
+            return;
+        } else {
+            res.setHeader('Content-Type', 'text/html; charset=utf-8');
+            res.statusCode = 200;
+            res.write(content);
+            res.end();
+        }
     });
-    // Sends the header immediately, no further modification is possible, e.g. res.setHeader has no effect
-    */
-    res.write('<html><h1>The server is working</h1></html>');
-    res.end();
 });
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // console.log('server.js last line', import.meta, testImport());
 // import.meta refers to this module; import.meta mentioned within testImport body refers to ./public/testImport.js file
+
+/*
+// Sending a hard-coded HTML
+const server = http.createServer((req, res) => {
+    const pathToResource = path.join(import.meta.dirname, 'public', 'index.html');
+    console.log('pathToResource', pathToResource);
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.statusCode = 200;
+    // Alternatively:
+    /Comment here
+    res.writeHead(
+        200,
+        {
+            'Content-Type': 'text/html; charset=utf-8'
+    });
+    // Sends the header immediately, no further modification is possible, e.g. res.setHeader has no effect
+    Comment ends here/
+    res.write('<html><h1>The server is working</h1></html>');
+    res.end();
+});
+*/
