f0fc4ac02df1b284becdb3893f750d84518edd49
Aleks Tyc <aleks.tyc@gmail.com>
2025-11-27 12:52:06 +0100

    Ex_0034 Databases I setup and simple queries

---
+++ Ex_0034/query.sql
@@ -0,0 +1,4 @@
+-- SELECT * FROM cars;
+-- SELECT brand, model, condition, year FROM cars;
+-- SELECT brand, model, condition, price FROM cars WHERE color = 'black';
+SELECT brand, model, condition, price FROM cars WHERE condition = 0;
