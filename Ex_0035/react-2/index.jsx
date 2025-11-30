99789840306edbdb6f6a08c1709dc74e81acb38d
Aleks Tyc <aleks.tyc@gmail.com>
2025-11-30 18:14:58 +0100

    Ex_0035 React.js Fun Facts project - composability II - Parent/Child

--- Ex_0035/react-2/index.jsx
+++ Ex_0035/react-2/index.jsx
@@ -3,26 +3,44 @@ import reactLogo from "/src/assets/react.svg";
 
 const rootElem = ReactDOM.createRoot(document.getElementById('root'));
 
+function Header() {
+    return (
+        <header>
+            <img src={reactLogo} alt="React Logo"></img>
+        </header>
+    );
+}
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Reasons</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterprise apps</li>
+            </ul>
+        </main>
+    );
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>C 20xx Urlyk von Jungingen development.</small>
+        </footer>
+    );
+}
+
 function Page() {
     return (
-        <div>
-            <header>
-                <img src={reactLogo} alt="React Logo"></img>
-            </header>
-            <main>
-                <h1>Reasons</h1>
-                <ul>
-                    <li>Was first released in 2013</li>
-                    <li>Was originally created by Jordan Walke</li>
-                    <li>Has well over 100k stars on GitHub</li>
-                    <li>Is maintained by Meta</li>
-                    <li>Powers thousands of enterprise apps</li>
-                </ul>
-            </main>
-            <footer>
-                <small>C 20xx Urlyk von Jungingen development.</small>
-            </footer>
-        </div>
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
     );
 }
 
