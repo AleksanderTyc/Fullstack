20c2d7a4096fc1816168b3875efd89c4fd6f7cfb
Aleks Tyc <aleks.tyc@gmail.com>
2025-11-30 18:09:47 +0100

    Ex_0035 React.js Fun Facts project - composability I

--- Ex_0035/react-2/index.jsx
+++ Ex_0035/react-2/index.jsx
@@ -4,14 +4,25 @@ import reactLogo from "/src/assets/react.svg";
 const rootElem = ReactDOM.createRoot(document.getElementById('root'));
 
 function Page() {
-    return(
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100k stars on GitHub</li>
-            <li>Is maintained by Meta</li>
-            <li>Powers thousands of enterprise apps</li>
-        </ul>
+    return (
+        <div>
+            <header>
+                <img src={reactLogo} alt="React Logo"></img>
+            </header>
+            <main>
+                <h1>Reasons</h1>
+                <ul>
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100k stars on GitHub</li>
+                    <li>Is maintained by Meta</li>
+                    <li>Powers thousands of enterprise apps</li>
+                </ul>
+            </main>
+            <footer>
+                <small>C 20xx Urlyk von Jungingen development.</small>
+            </footer>
+        </div>
     );
 }
 
