816f83afcbea6fd618e2cab0b204c177cd303ebc
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-03 18:43:36 +0100

    Ex_0036 React.js Chef Claude - form submit basic handling

--- Ex_0036/react-5/Header.jsx
+++ Ex_0036/react-5/Header.jsx
@@ -1,9 +1,10 @@
 import chefLogo from "./images/chef-claude-icon.png";
+// <img onMouseLeave={e => console.log('onMouseLeave')} onMouseEnter={e => console.log('onMouseEnter')} src={chefLogo} alt="Chef Claude Logo"></img>
 
 function Header() {
     return (
         <header>
-            <img onMouseLeave={e => console.log('onMouseLeave')} onMouseEnter={e => console.log('onMouseEnter')} src={chefLogo} alt="Chef Claude Logo"></img>
+            <img src={chefLogo} alt="Chef Claude Logo"></img>
             <span>Chef Claude</span>
         </header>
     );
--- Ex_0036/react-5/Main.jsx
+++ Ex_0036/react-5/Main.jsx
@@ -1,10 +1,16 @@
 function Main() {
+    const ingredients = ['Chicken', 'Oregano', 'Tomatoes'];
+
+    // This is a very basic, simplistic approach. The page reloads.
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient"></input>
+            <form onSubmit={() => console.log("Form submitted")} className="add-ingredient-form">
+                <input name="ingredient" type="text" placeholder="e.g. oregano" aria-label="Add ingredient"></input>
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingredients.map((ingr, idx) => <li key={idx}>{ingr}</li>)}
+            </ul>
         </main>
     );
 }
