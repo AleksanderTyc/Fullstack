7657c90e102cc113b3450ebdf4cb051a98d32b82
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-03 18:54:26 +0100

    Ex_0036 React.js Chef Claude - form submit - reload prevented

--- Ex_0036/react-5/Main.jsx
+++ Ex_0036/react-5/Main.jsx
@@ -2,9 +2,17 @@ function Main() {
     const ingredients = ['Chicken', 'Oregano', 'Tomatoes'];
 
     // This is a very basic, simplistic approach. The page reloads.
+    // <form onSubmit={() => console.log("Form submitted")}
+
+    // Disable page reload.
+    function handleOnSubmit(evt) {
+        evt.preventDefault();
+        console.log("Form submitted", evt.target);
+    }
+
     return (
         <main>
-            <form onSubmit={() => console.log("Form submitted")} className="add-ingredient-form">
+            <form onSubmit={handleOnSubmit} className="add-ingredient-form">
                 <input name="ingredient" type="text" placeholder="e.g. oregano" aria-label="Add ingredient"></input>
                 <button>Add ingredient</button>
             </form>
