507a57891675fff35f9977e1e9faa5580122ee3d
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-03 21:24:00 +0100

    Ex_0036 React.js Chef Claude - form submit - useState to update ingredients - II

--- Ex_0036/react-5/Main.jsx
+++ Ex_0036/react-5/Main.jsx
@@ -9,6 +9,28 @@ function Main() {
 
     // Use State to manage the array of ingredients
     const [ingredients, setIngredients] = React.useState(['Chicken', 'Oregano', 'Tomatoes']);
+    // Remember - modifying state variable `ingredients` directly is not allowed.
+    // We can do it by referring to `ingredients`:
+    // setIngredients(Array.of(...ingredients, newIngredient));
+    // We can also use a callback function, which receives the previous value of state:
+    // setIngredients(previousIngredients => Array.of(...previousIngredients, newIngredient));
+    // Initially I thought that the callback's argument is different from the state itself, so this should work:
+    // setIngredients(previousIngredients => {
+    //     previousIngredients.push(newIngredient);
+    //     return previousIngredients;
+    // });
+    // But it does not. Also what does not work is:
+    // setCount(count => count++);
+    // where count is integer.
+    // Difference:
+    // setCount(count +1);
+    // setCount(count +1);
+    // As we know it "executes" only once. Whereas:
+    // setCount(count => count+1);
+    // setCount(count => count+1);
+    // will result in count increase of 2.
+    // The course seems to suggest against using the state directly. Instead, if we need the previous state
+    // to determine the new state, we should use callback function.
 
     function handleOnSubmit(evt) {
         evt.preventDefault();
@@ -16,7 +38,7 @@ function Main() {
         const newIngredient = formData.get("ingredient");
         // console.log( ...ingredients);
         console.log("Form submitted", newIngredient, Array.of(...ingredients, newIngredient));
-        setIngredients(Array.of(...ingredients, newIngredient));
+        setIngredients(previousIngredients => Array.of(...previousIngredients, newIngredient));
     }
 
     return (
