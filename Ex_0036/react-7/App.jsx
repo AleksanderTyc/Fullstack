d0405c70ce903bed4ede6a5b756c1604ffce0471
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-08 14:50:08 +0100

    Ex_0036 React.js Sound Pads - dynamic className

--- Ex_0036/react-7/App.jsx
+++ Ex_0036/react-7/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react'
 import padsData from './pads.js';
 
-function Pad({ colour }) {
-  return (<button style={{ backgroundColor: colour }}>{colour}</button>);
+function Pad({ colour, toggle }) {
+  function toggleButton() {
+    console.log(`* I * toggleButton clicked, colour: ${colour}`);
+  }
+  return (
+    <button
+      style={{ backgroundColor: colour }}
+      className={toggle ? 'on' : undefined}
+      onClick={toggleButton}
+    >
+      {colour}
+    </button>
+  );
 }
 
 function App(props) {
@@ -10,7 +21,7 @@ function App(props) {
   return (
     <main style={props.darkMode ? { backgroundColor: "#222" } : { backgroundColor: "#ccc" }}>
       <div className='pad-container'>
-        {pads.map(pad => <Pad key={pad.id} colour={pad.color} />)}
+        {pads.map(pad => <Pad key={pad.id} colour={pad.color} toggle={pad.on} />)}
       </div>
     </main>
   );
