13a2c2b45280196b7f9005d20a0fb603738b33e2
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-08 13:49:49 +0100

    Ex_0036 React.js Sound Pads - dynamic styling II

--- Ex_0036/react-7/App.jsx
+++ Ex_0036/react-7/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import padsData from './pads.js';
 
+function Pad({ colour }) {
+  return (<button style={{ backgroundColor: colour }}>{colour}</button>);
+}
+
 function App(props) {
   const [pads, setPads] = useState(padsData);
   return (
     <main style={props.darkMode ? { backgroundColor: "#222" } : { backgroundColor: "#ccc" }}>
       <div className='pad-container'>
-        {pads.map(pad => {
-          return (<button style={{ backgroundColor: pad.color }} key={pad.id}></button>);
-        })}
+        {pads.map(pad => <Pad key={pad.id} colour={pad.color} />)}
       </div>
     </main>
   );
