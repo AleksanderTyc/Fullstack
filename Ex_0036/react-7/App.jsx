ad5ecd3602e6fd34dabc9d544959729624fd3f92
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-08 15:43:27 +0100

    Ex_0036 React.js Sound Pads - inherited state - my first take - toggleButton local to App component, passed to Pads

--- Ex_0036/react-7/App.jsx
+++ Ex_0036/react-7/App.jsx
@@ -1,42 +1,33 @@
 import { useState } from 'react'
 import padsData from './pads.js';
 
-function Pad({ id, pads, handlePads }) {
+function Pad({ pad, toggle }) {
 
-  const padIndex = pads.findIndex(pad => pad.id === id);
-  console.log(`* I * Rendering id ${id}, index ${padIndex}, colour ${pads[padIndex].color}`);
-
-  // const [localState, setLocalState] = useState(toggle);
-
-  function toggleButton() {
-    console.log(`* I * toggleButton clicked, colour: ${pads[padIndex].color}`);
-    // setLocalState(prev => !prev);
-    handlePads(prev => {
-      const newPads = prev.slice(0);
-      newPads[padIndex].on = !newPads[padIndex].on;
-      return newPads;
-    });
-    // Alternative:
-    // handlePads(prev => prev.map((pad,index) => index === padIndex ? {...pad, on: !pad.on} : pad ));
-  }
+  console.log(`* I * Rendering id ${pad.id},colour ${pad.color}`);
 
   return (
     <button
-      style={{ backgroundColor: pads[padIndex].color }}
-      className={pads[padIndex].on ? 'on' : undefined}
-      onClick={toggleButton}
+      style={{ backgroundColor: pad.color }}
+      className={pad.on ? 'on' : undefined}
+      onClick={() => toggle(pad.id)}
     >
-      {pads[padIndex].color}
+      {pad.color}
     </button>
   );
 }
 
 function App(props) {
   const [pads, setPads] = useState(padsData);
+
+  function toggleButton(padId) {
+    console.log(`* I * toggleButton clicked, id: ${padId}`);
+    setPads(prev => prev.map((pad) => pad.id === padId ? {...pad, on: !pad.on} : pad ));
+  }
+
   return (
     <main style={props.darkMode ? { backgroundColor: "#222" } : { backgroundColor: "#ccc" }}>
       <div className='pad-container'>
-        {pads.map(pad => <Pad key={pad.id} id={pad.id} pads={pads} handlePads={setPads} />)}
+        {pads.map(pad => <Pad key={pad.id} pad={pad} toggle={toggleButton} />)}
       </div>
     </main>
   );
