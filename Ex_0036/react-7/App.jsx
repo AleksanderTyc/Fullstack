5d31f2f705f6075d75bbcfa768ef4f18ffed7bd9
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-08 15:28:01 +0100

    Ex_0036 React.js Sound Pads - inherited state - my first take - toggleButton local to Pad component

--- Ex_0036/react-7/App.jsx
+++ Ex_0036/react-7/App.jsx
@@ -1,22 +1,32 @@
 import { useState } from 'react'
 import padsData from './pads.js';
 
-function Pad({ colour, toggle }) {
+function Pad({ id, pads, handlePads }) {
 
-  const [localState, setLocalState] = useState(toggle);
+  const padIndex = pads.findIndex(pad => pad.id === id);
+  console.log(`* I * Rendering id ${id}, index ${padIndex}, colour ${pads[padIndex].color}`);
+
+  // const [localState, setLocalState] = useState(toggle);
 
   function toggleButton() {
-    console.log(`* I * toggleButton clicked, colour: ${colour}`);
-    setLocalState(prev => !prev);
+    console.log(`* I * toggleButton clicked, colour: ${pads[padIndex].color}`);
+    // setLocalState(prev => !prev);
+    handlePads(prev => {
+      const newPads = prev.slice(0);
+      newPads[padIndex].on = !newPads[padIndex].on;
+      return newPads;
+    });
+    // Alternative:
+    // handlePads(prev => prev.map((pad,index) => index === padIndex ? {...pad, on: !pad.on} : pad ));
   }
 
   return (
     <button
-      style={{ backgroundColor: colour }}
-      className={localState ? 'on' : undefined}
+      style={{ backgroundColor: pads[padIndex].color }}
+      className={pads[padIndex].on ? 'on' : undefined}
       onClick={toggleButton}
     >
-      {colour}
+      {pads[padIndex].color}
     </button>
   );
 }
@@ -26,7 +36,7 @@ function App(props) {
   return (
     <main style={props.darkMode ? { backgroundColor: "#222" } : { backgroundColor: "#ccc" }}>
       <div className='pad-container'>
-        {pads.map(pad => <Pad key={pad.id} colour={pad.color} toggle={pad.on} />)}
+        {pads.map(pad => <Pad key={pad.id} id={pad.id} pads={pads} handlePads={setPads} />)}
       </div>
     </main>
   );
