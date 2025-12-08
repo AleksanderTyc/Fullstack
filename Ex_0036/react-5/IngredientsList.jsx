572f9c1d431a4f5ea8e228de28e382500a892458
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-08 20:26:04 +0100

    Ex_0036 React.js Chef Claude - inherited state

---
+++ Ex_0036/react-5/IngredientsList.jsx
@@ -0,0 +1,17 @@
+function IngredientsList(props) {
+    const ingredientsListItems = props.listIngredients.map((ingr, idx) => <li key={idx}>{ingr}</li>);
+    return (
+        <section>
+            <h2>Ingredients on hand:</h2>
+            <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+            {ingredientsListItems.length > 3 && <div className="get-recipe-container">
+                <div>
+                    <h3>Ready for a recipe?</h3>
+                    <p>Generate a recipe from your list of ingredients.</p>
+                </div>
+                <button onClick={props.handleGetRecipe}>Get a recipe</button>
+            </div>}
+        </section>);
+}
+
+export { IngredientsList };
