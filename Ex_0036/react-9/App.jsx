2ea4bf10e3ea53df71b62d57f136d4f9c47c0922
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-13 14:49:05 +0100

    Ex_0036 React.js useEffect exercise, with Star Wars - initial

--- Ex_0036/react-9/App.jsx
+++ Ex_0036/react-9/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 function App() {
+    const [starWarsData, setStarWarsData] = React.useState({});
     const [count, setCount] = React.useState(0);
 
     console.log('Rendered');
@@ -9,6 +10,7 @@ function App() {
         <>
             <h2>The count is {count}</h2>
             <button onClick={() => setCount(prev => prev + 1)}>Add</button>
+            <pre>{JSON.stringify(starWarsData)}</pre>
         </>
     );
 }
