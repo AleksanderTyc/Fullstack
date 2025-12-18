aa52eae8fbd3207959d29eb05c83ee21ad6f3e78
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-18 18:28:15 +0100

    Ex_0038 - TS - Pizza app - JS with obvious errors

--- Ex_0038/index.js
+++ Ex_0038/index.js
@@ -6,9 +6,33 @@ const menu = [
 
 ];
 
+// This won't work - cash is modified later in the code
 const cashInRegister = 100;
 const orderQueue = [];
+// This won't work - nextOrderId is modified later in the code
+const nextOrderId = 1;
+// Changing both const's into let's makes the code usable, but this is not the point of the exercise.
 
 function addNewPizza(newPizza) {
     menu.push(newPizza);
 }
+
+function placeOrder(pizzaName) {
+    const pizzaOrder = menu.filter(elem => elem.name === pizzaName)[0];
+    // Their way: menu.find(elem => elem.name === pizzaName);
+    cashInRegister += pizzaOrder.price;
+    const newOrder = { pizza: pizzaOrder, status: "ordered", orderId: nextOrderId++ };
+    orderQueue.push(newOrder);
+    return newOrder;
+}
+
+function completeOrder(orderId) {
+    const orderIndex = orderQueue.findIndex(elem => elem.orderId === orderId);
+    const requestedOrder = orderQueue[orderIndex];
+    orderQueue[orderIndex].status = 'complete';
+    return requestedOrder;
+}
+// Notice that
+// const requestedOrder = orderQueue[orderIndex];
+// builds a reference to the array's member, not a copy of it.
+// In result, requestOrder.status === 'complete' at the end of the function (returned value).
