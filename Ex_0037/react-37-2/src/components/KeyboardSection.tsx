0013f2ac8d54b3e04d0a9fa29d5108946148f53c
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-21 16:53:50 +0100

    Ex_0037 - React & TS - Endgame - Keyboard JSX component fully typed

--- Ex_0037/react-37-2/src/components/KeyboardSection.tsx
+++ Ex_0037/react-37-2/src/components/KeyboardSection.tsx
@@ -8,18 +8,22 @@ function KeyboardSection(props: {
     gLetters: Array<LetterGuess>
     handler: (evt: any) => void
 }): JSX.Element {
-    // gLetters={guessedLetters} handler
-    const alphabet = [];
-    for (let index = 65; index < 91; index++) {
+    type CharacterObject = { idx: number, character: string };
+    const alphabet: Array<CharacterObject> = [];
+    for (let index: number = 65; index < 91; index++) {
         alphabet.push({
             idx: index,
             character: String.fromCharCode(index)
         });
     }
-    const correctGuessed = props.gLetters.filter(elem => elem.guess).map(elem => elem.character);
-    const incorrectGuessed = props.gLetters.filter(elem => !elem.guess).map(elem => elem.character);
-    const keyboardButtons = alphabet.map(
-        charObj => <button
+    const correctGuessed = props.gLetters
+        .filter((elem: LetterGuess): boolean => elem.guess)
+        .map((elem: LetterGuess): string => elem.character);
+    const incorrectGuessed = props.gLetters
+        .filter((elem: LetterGuess): boolean => !elem.guess)
+        .map((elem: LetterGuess): string => elem.character);
+    const keyboardButtons: Array<JSX.Element> = alphabet.map(
+        (charObj: CharacterObject): JSX.Element => <button
             key={charObj.idx}
             onClick={props.handler}
             disabled={props.gameOver}
