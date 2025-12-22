0a185750c62b3335a45eb27bf19555f1b29784fd
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-22 22:08:03 +0100

    Ex_0037 4 Next.js Printforge layout navbar

--- Ex_0037/react-37-4/app/layout.tsx
+++ Ex_0037/react-37-4/app/layout.tsx
@@ -8,7 +8,30 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <header className="w-full bg-white">
+          <nav className="flex justify-between px-6 py-4">
+            <div className="relative">
+              {/* Desktop icon */}
+              <img
+                src="/printforge-logo.svg"
+                alt="printforge logo home"
+                className="w-[200px] h-auto hidden md:block"
+              />
+              {/* Mobile icon */}
+              <img
+                src="/printforge-logo-icon.svg"
+                alt="printforge logo icon home"
+                className="w-[40px] h-auto hidden md:hidden"
+              />
+            </div>
+            <ul className="flex items-center gap-2.5 uppercase">
+              <li>3D models</li>
+              <li>about</li>
+            </ul>
+          </nav>
+        </header>
         {children}
+        (C) 1410 Urlyk von Jungingen
       </body>
     </html>
   );
