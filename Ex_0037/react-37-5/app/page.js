5b4f28e8ef97f19716be4e77fdd00aa1c710b940
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-30 20:10:43 +0100

    Ex_0037 5 Next.js CatFacts initial setup

---
+++ Ex_0037/react-37-5/app/page.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+async function getCatFact() {
+  return (
+    { fact: 'Cats step both left legs, then both right legs, when they walk.' }
+  );
+}
+
+export default async function Home() {
+  const catFact = await getCatFact();
+  return (
+    <div className="page">
+      <main className="main">
+        <h1>Cat Facts</h1>
+        <div className="fact-card">
+          <p className="fact-text">{catFact.fact}</p>
+        </div>
+      </main>
+    </div>
+  );
+}
