36dfa23aa999d033fa12f6abae836570bb082ff5
Aleks Tyc <aleks.tyc@gmail.com>
2025-12-30 21:22:28 +0100

    Ex_0037 5 Next.js CatFacts getCatFacts (multiple)

--- Ex_0037/react-37-5/app/page.js
+++ Ex_0037/react-37-5/app/page.js
@@ -1,12 +1,20 @@
-export const dynamic = 'force-dynamic';
+// export const dynamic = 'force-dynamic';
 
 
 async function getCatFact() {
   // const res = await fetch('https://catfact.ninja/fact', { cache: 'no-store' }); // SSR
-  // const res = await fetch('https://catfact.ninja/fact'); // dev: SSR, prod: SSG
-  const res = await fetch('https://catfact.ninja/fact', { cache: 'force-cache' }); // SSG when together with export const dynamic
+  const res = await fetch('https://catfact.ninja/fact'); // dev: SSR, prod: SSG
+  // const res = await fetch('https://catfact.ninja/fact', { cache: 'force-cache' }); // SSG when together with export const dynamic
   const data = res.json();
   return data;
+}
+
+async function getCatFacts() {
+  // const res = await fetch('https://catfact.ninja/facts', { cache: 'no-store' }); // SSR
+  const res = await fetch('https://catfact.ninja/facts'); // dev: SSR, prod: SSG
+  // const res = await fetch('https://catfact.ninja/facts', { cache: 'force-cache' }); // SSG when together with export const dynamic
+  const data = await res.json();
+  return data.data;
 }
 
 async function getCatFactStatic() {
@@ -16,16 +24,20 @@ async function getCatFactStatic() {
 }
 
 export default async function Home() {
-  const catFact = await getCatFact();
+  // const catFact = await getCatFact();
+  const catFacts = await getCatFacts();
   const timeStamp = new Date().toLocaleString();
+
+  const renderedCatFacts = catFacts.map((fact, index) => <div key={index} className="fact-card">
+    <p className="fact-text">{fact.fact}</p>
+  </div>
+  );
   return (
     <div className="page">
       <main className="main">
         <h1>Cat Facts</h1>
-        <div className="fact-card">
-          <p className="timestamp">Rendered at {timeStamp}</p>
-          <p className="fact-text">{catFact.fact}</p>
-        </div>
+        <p className="timestamp">Rendered at {timeStamp}</p>
+        {renderedCatFacts}
       </main>
     </div>
   );
